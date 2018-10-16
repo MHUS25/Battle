57a9f028cb268dc8f7f3691d0021b0d6609ef20d
Muna <mmhussein125@gmail.com>
2018-10-16 18:55:07 +0100

    DRYing up code with helpers

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'See Player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Jill'
-    fill_in :player_2_name, with: 'Jack'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Jack: 100HP'
   end
 end
