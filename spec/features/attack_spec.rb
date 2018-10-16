44bd2ae5bf642b4630df02f54bf6d64166ccd35a
Muna <mmhussein125@gmail.com>
2018-10-16 22:40:42 +0100

    Refactored controller to make it skinny & added OK link

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -9,7 +9,7 @@ feature 'Attack' do
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    # click_button 'OK'
+    click_link 'OK'
     expect(page).not_to have_content 'Jack: 100HP'
     expect(page).to have_content 'Jack: 90HP'
   end
