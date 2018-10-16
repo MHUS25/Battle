ab67dd27f920388507f99ee59166c4a027eabfb4
Muna <mmhussein125@gmail.com>
2018-10-16 22:08:47 +0100

    User Story 4 - Implementing hit points

--- app.rb
+++ app.rb
@@ -1,8 +1,7 @@
 require 'sinatra/base'
-# require 'Player'
+require './lib/Player'
 
 class Battle < Sinatra::Base
-  # enable :sessions
 
   get '/' do
     erb :index
@@ -15,14 +14,15 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
     erb :attack
   end
 
