049ed458255e56e6ecc2730468a9b97a4c9e5923
Muna <mmhussein125@gmail.com>
2018-10-16 18:48:28 +0100

    User Story 2 - Viewing hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,9 @@
-feature 'Enter names' do
-  scenario 'submitting names' do
+feature 'View hit points' do
+  scenario 'See Player 2 hit points' do
     visit('/')
     fill_in :player_1_name, with: 'Jill'
     fill_in :player_2_name, with: 'Jack'
     click_button 'Submit'
-    expect(page).to have_content 'Jill vs. Jack'
+    expect(page).to have_content 'Jack: 100HP'
   end
 end
