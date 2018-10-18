334f7f01a47e34549430145dbc4cc6128539acfe
Muna <mmhussein125@gmail.com>
2018-10-18 11:56:00 +0100

    User Story 6 - Player 2 hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,12 @@
 feature 'View hit points' do
+
   scenario 'See Player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Jack: 100HP'
+  end
+
+  scenario 'See Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Jill: 100HP'
   end
 end
