d55b3d06a3474bbce8877827c2ffefacc63b1085
Muna <mmhussein125@gmail.com>
2018-10-18 15:30:09 +0100

    Play again feature

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -3,10 +3,21 @@ feature 'Game over' do
     scenario 'Jill kills Jack' do
       sign_in_and_play
       super_attack
-      expect(page).to have_content 'Jack is dead'
+      expect(page).to have_content 'Jack is dead - GAME OVER!'
     end
   end
+
+  scenario 'Play again' do
+    sign_in_and_play
+    super_attack
+    expect(page).to have_button 'Play again'
+  end
+
+  scenario 'Starts new game' do
+    sign_in_and_play
+    super_attack
+    click_button 'Play again'
+    expect(page).to have_content 'Sign in to play'
+  end
+
 end
-
-
-  
