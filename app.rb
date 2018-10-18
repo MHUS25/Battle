754f2861b0c744af4b5e72da597e88b115942ae6
rbbri <robin.s.johnson@gmail.com>
2018-10-18 15:04:07 +0100

    Removed global variables

--- app.rb
+++ app.rb
@@ -12,30 +12,31 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
+  before do
+    @game = Game.instance
+  end
+
+
   get '/play' do
-    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @game = $game
     @game.attack(@game.opponent_of(@game.current_turn))
     erb :attack
   end
 
   post '/switch-turns' do
-    @game = $game
     @game.switch_turns
     redirect('/game_over') if @game.game_over?
     redirect('/play')
   end
 
   get '/game_over' do
-    @game = $game
     erb :game_over
   end
 
