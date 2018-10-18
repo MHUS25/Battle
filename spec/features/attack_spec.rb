9e89ffa2cdb76956e1bf70e8f09fc39ee8936096
Muna <mmhussein125@gmail.com>
2018-10-18 22:21:49 +0100

    Refactoring

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,8 +8,7 @@ feature 'Attack' do
 
   scenario 'be attacked by Player 2' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     click_button 'Attack'
     expect(page).to have_content 'Jack attacked Jill'
   end
@@ -17,26 +16,22 @@ feature 'Attack' do
   scenario 'reduce Player 2 HP by 0' do
     allow_any_instance_of(Player).to receive(:rand_points).and_return(0)
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     expect(page).to have_content 'Jack: 100HP'
   end
 
   scenario 'reduce Player 1 HP by 0' do
     allow_any_instance_of(Player).to receive(:rand_points).and_return(0)
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
+    attack_and_confirm
     expect(page).to have_content 'Jill: 100HP'
   end
 
   scenario 'reduce Player 2 HP by 10' do
     allow_any_instance_of(Player).to receive(:rand_points).and_return(10)
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     expect(page).not_to have_content 'Jack: 100HP'
     expect(page).to have_content 'Jack: 90HP'
   end
@@ -44,10 +39,8 @@ feature 'Attack' do
   scenario 'reduce Player 1 HP by 10' do
     allow_any_instance_of(Player).to receive(:rand_points).and_return(10)
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
+    attack_and_confirm
     expect(page).not_to have_content 'Jill: 100HP'
     expect(page).to have_content 'Jill: 90HP'
   end
@@ -55,8 +48,7 @@ feature 'Attack' do
   scenario 'reduce Player 2 HP by 20' do
     allow_any_instance_of(Player).to receive(:rand_points).and_return(20)
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     expect(page).not_to have_content 'Jack: 100HP'
     expect(page).to have_content 'Jack: 80HP'
   end
@@ -64,10 +56,8 @@ feature 'Attack' do
   scenario 'reduce Player 1 HP by 20' do
     allow_any_instance_of(Player).to receive(:rand_points).and_return(20)
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
+    attack_and_confirm
     expect(page).not_to have_content 'Jill: 100HP'
     expect(page).to have_content 'Jill: 80HP'
   end
@@ -75,8 +65,7 @@ feature 'Attack' do
   scenario 'reduce Player 2 HP by 50' do
     allow_any_instance_of(Player).to receive(:rand_points).and_return(50)
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     expect(page).not_to have_content 'Jack: 100HP'
     expect(page).to have_content 'Jack: 50HP'
   end
@@ -84,10 +73,8 @@ feature 'Attack' do
   scenario 'reduce Player 1 HP by 50' do
     allow_any_instance_of(Player).to receive(:rand_points).and_return(50)
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
+    attack_and_confirm
     expect(page).not_to have_content 'Jill: 100HP'
     expect(page).to have_content 'Jill: 50HP'
   end
--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -1,24 +1,21 @@
 feature 'Game over' do
+  before do
+    allow_any_instance_of(Player).to receive(:rand_points).and_return(100)
+    sign_in_and_play
+    attack_and_confirm
+  end
+
     context 'when Player 1 reaches 0 HP first' do
       scenario 'Player 1 loses' do
-        allow_any_instance_of(Player).to receive(:rand_points).and_return(100)
-        sign_in_and_play
-        attack_and_confirm
-        expect(page).to have_content 'Jack is dead - GAME OVER!'
+        expect(page).to have_content 'Jack loses - GAME OVER!'
       end
     end
-    
+
     scenario 'Can play again' do
-      allow_any_instance_of(Player).to receive(:rand_points).and_return(100)
-      sign_in_and_play
-      attack_and_confirm
       expect(page).to have_button 'Play again'
     end
 
     scenario 'Can start a new game' do
-      allow_any_instance_of(Player).to receive(:rand_points).and_return(100)
-      sign_in_and_play
-      attack_and_confirm
       click_button 'Play again'
       expect(page).to have_content 'Sign in to play'
     end
